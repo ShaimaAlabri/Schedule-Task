28856497ffeaf687bed9336447393647b06294dd
unknown <shaima96alabri@hotmail.com>
2023-05-31 00:20:30 -0700

    solved problem 1&2

--- src/main/java/com/example/Schedule/Task/Schedule.java
+++ src/main/java/com/example/Schedule/Task/Schedule.java
@@ -1,4 +1,22 @@
 package com.example.Schedule.Task;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Schedule {
+    //Problem 1: Write a CRON expression that runs a job every five minutes.
+    @Scheduled(cron = "0 */5 * * * *")
+    public void runTask() {
+        // Your task logic here
+        System.out.println("Task executed every 5 minutes");
+    }
+// Problem 2: Write a CRON expression that runs a job every hour.
+@Scheduled(cron = "0 0 * * * *")
+public void runTask1() {
+    // Your task logic here
+    System.out.println("Task executed every 5 minutes");
+}
+
+
 }
